09838363d9d69d48d06d444c40ab8089747ad158
Julio Enrique <101289401+kikin-mentor@users.noreply.github.com>
2025-06-13 13:57:58 +0000

    Base structure

--- app.py
+++ app.py
@@ -12,25 +12,6 @@ class Index:
     def GET(self):
         return render.index()
     
-    def POST(self):
-        formulario = web.input(Index)
-        print(formulario)
-        formulario=web.input()
-        num1 = int(input=="num1")
-        num2 = int(input=="num2")
-        oper = formulario.button()
-
-        if(oper=="suma"):
-            result=num1 + num2
-        elif(oper=="resta"):
-            result=num1 - num2
-        elif(oper=="multiplicacion"):
-            result=num1 * num2
-        elif(oper=="division"):
-            result=num1 / num2
-
-        print(result)
-        return render.index(num1, num2, result)
     
 if __name__ == "__main__":
     app.run()
